3ffd1d9e9f40a436680c3bfd60a4e91501517340
nzodalandry <nzodalandry@gmail.com>
2018-09-28 20:51:09 -0400

    orders controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  post 'orders/create', to: 'orders#create'
+  get 'orders/index', to: 'orders#index'
   resources :pizzas
   resources :testjson 
-  resources :orders
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
